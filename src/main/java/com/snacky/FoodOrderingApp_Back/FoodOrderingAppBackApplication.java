c9fc2a00183d900ee49ccc9383250caa16c420bd
kaan <kaanakman42@gmail.com>
2024-11-25 17:30:00 +0300

    structure files

---
+++ src/main/java/com/snacky/FoodOrderingApp_Back/FoodOrderingAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.snacky.FoodOrderingApp_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderingAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderingAppBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snacky/FoodOrderingApp_Back/FoodOrderingAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snacky.FoodOrderingApp_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
