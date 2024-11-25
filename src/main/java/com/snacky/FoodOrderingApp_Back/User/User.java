7f76acc8a39adcacaa60b4e7e9479ff522521911
kaan <kaanakman42@gmail.com>
2024-11-25 18:17:01 +0300

    structure files

--- src/main/java/com/snacky/FoodOrderingApp_Back/User/User.java
+++ src/main/java/com/snacky/FoodOrderingApp_Back/User/User.java
@@ -1,4 +1,35 @@
 package com.snacky.FoodOrderingApp_Back.User;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "snacky_user")
+@Data //We have using lombok, hence we don't need to create getters and setters etc.
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false)
+    private Long id;
+
+    @Column(name = "first_name",nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "phone_number",nullable = false)
+    private String phoneNumber;
+
+    @UuidGenerator
+    @Column(name = "public_id", nullable = false)
+    private UUID publicId;
+
 }
