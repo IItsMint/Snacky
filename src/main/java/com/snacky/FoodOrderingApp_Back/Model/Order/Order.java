7c0d3b34071831421c4b892043a71e200912c3cc
kaan <kaanakman42@gmail.com>
2024-11-28 20:01:32 +0300

    order & restaurant Entity structured

--- src/main/java/com/snacky/FoodOrderingApp_Back/Model/Order/Order.java
+++ src/main/java/com/snacky/FoodOrderingApp_Back/Model/Order/Order.java
@@ -1,7 +1,12 @@
 package com.snacky.FoodOrderingApp_Back.Model.Order;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.snacky.FoodOrderingApp_Back.Model.Address.Address;
+import com.snacky.FoodOrderingApp_Back.Model.Restaurant.Restaurant;
 import com.snacky.FoodOrderingApp_Back.Model.User.User;
 import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
 public class Order {
@@ -9,7 +14,19 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne//relation between order and user.
     private User customer;
+
+    @JsonIgnore//whenever, we fetch the order object, we don't need restaurant object inside order object.
+    @ManyToOne//relation between order and restaurant.
+    private Restaurant restaurant;
+
+    private Long total;
+    private String status;
+    private Date date;
+
+    @ManyToOne//Relation between order and address.
+    private Address address;
+
 
 }
