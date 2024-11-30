b5c804b2085d3c289adc018b5c44471b148b9bdc
kaan <kaanakman42@gmail.com>
2024-11-30 23:06:24 +0300

    ingredients structure started

--- src/main/java/com/snacky/FoodOrderingApp_Back/Model/Products/IngredientCategory.java
+++ src/main/java/com/snacky/FoodOrderingApp_Back/Model/Products/IngredientCategory.java
@@ -4,21 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Ingredients {
-
+public class IngredientCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    private String name;
-    private  ingredientCategory category;
-    private int quantity;
 }
