7d0b1c642b87405c2aa426f90d41a7d31fe02553
kaan <kaanakman42@gmail.com>
2024-12-04 23:46:07 +0300

    cors configuration for security

--- src/main/java/com/snacky/FoodOrderingApp_Back/Config/AppConfig.java
+++ src/main/java/com/snacky/FoodOrderingApp_Back/Config/AppConfig.java
@@ -1,10 +1,18 @@
 package com.snacky.FoodOrderingApp_Back.Config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -13,7 +21,36 @@ public class AppConfig {
 
     @Bean
     SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests(Authorize -> Authorize
+                        .requestMatchers("/api/admin/**").hasAnyRole("ADMIN", "RESTAURANT_OWNER")//Any end point starts with api-admin, only available for owners and admin.
+                        .requestMatchers("/api/**").authenticated()// any end point starts with api user can access with token.
+                        .anyRequest().permitAll()
+                ).addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
         return null;
+    }
+
+    //In this part we are giving access key to specific front end urls to reach backend.
+    private CorsConfigurationSource corsConfigurationSource() {
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(Arrays.asList(
+                        "https://localhost:4200"
+                ));
+                config.setAllowedMethods(Collections.singletonList("*"));//in here we are giving permission for all the features.
+                config.setAllowCredentials(true);
+                config.setAllowedHeaders(Collections.singletonList("*"));
+                config.setExposedHeaders(Arrays.asList("Authorization"));
+                config.setMaxAge(3600L);
+                return config;
+            }
+        };
     }
 
 }
