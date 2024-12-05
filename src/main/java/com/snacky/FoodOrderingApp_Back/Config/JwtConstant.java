4a767bbd7d807f55682b2db0c66169ed81fae388
kaan <kaanakman42@gmail.com>
2024-12-05 23:53:48 +0300

    jwt initialized

---
+++ src/main/java/com/snacky/FoodOrderingApp_Back/Config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.snacky.FoodOrderingApp_Back.Config;
+
+public class JwtConstant {
+
+    //Let's create random secret key string.
+    public static final String JWT_SECRET = "Y7AtZyuJuPxcAZyWa9VlQUW";
+    public static final String JWT_HEADER = "Authorization";
+}
