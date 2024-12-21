1397bcc4b7706837e8ead41378980acefd6ec246
kaan <kaanakman42@gmail.com>
2024-12-21 20:54:56 +0300

    Product DTO implemented.

--- src/main/java/com/snacky/FoodOrderingApp_Back/Dto/CreateProductRequest.java
+++ src/main/java/com/snacky/FoodOrderingApp_Back/Dto/CreateProductRequest.java
@@ -1,4 +1,27 @@
 package com.snacky.FoodOrderingApp_Back.Dto;
 
+import com.snacky.FoodOrderingApp_Back.Model.Product.Category;
+import com.snacky.FoodOrderingApp_Back.Model.Product.Ingredients;
+import com.snacky.FoodOrderingApp_Back.Model.Product.Product;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class CreateProductRequest {
+
+    private String name;
+    private String description;
+    private Long price;
+    private Integer quantity;
+    private Long restaurantId;
+    private Category category;
+
+    private List<Ingredients> ingredients;
+    private List<String> image;
+
+
+    private boolean isVegan;
+    private boolean isSeasonal;
+    private boolean isAvailable;
 }
