113986f4213474e9622ba6cc7e8f8e5cd29b7444
kaan <kaanakman42@gmail.com>
2024-12-23 14:57:56 +0300

    customer and admin controllers for products structured, create product method implemented in admin controller.

---
+++ src/main/java/com/snacky/FoodOrderingApp_Back/Controller/AdminProductController.java
@@ -0,0 +1,45 @@
+package com.snacky.FoodOrderingApp_Back.Controller;
+
+import com.snacky.FoodOrderingApp_Back.Dto.CreateProductRequest;
+import com.snacky.FoodOrderingApp_Back.Model.Product.Product;
+import com.snacky.FoodOrderingApp_Back.Model.Restaurant.Restaurant;
+import com.snacky.FoodOrderingApp_Back.Model.User.User;
+import com.snacky.FoodOrderingApp_Back.Service.ProductService;
+import com.snacky.FoodOrderingApp_Back.Service.RestaurantService;
+import com.snacky.FoodOrderingApp_Back.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin/products")
+public class AdminProductController {
+
+    //first lets call service layers that we are gonna route.
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody CreateProductRequest request,
+                                                 @RequestHeader("Authorization") String jwt) throws Exception{
+
+        //first lets get user,
+        User user = userService.findByJwtToken(jwt);
+
+        //now lets find restaurant,
+        Restaurant restaurant = restaurantService.findRestaurantById(request.getRestaurantId());
+
+        //now we can create the product,
+        Product product = productService.addProduct(request, request.getCategory(), restaurant);
+
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/snacky/FoodOrderingApp_Back/Controller/CustomerProductController.java
@@ -0,0 +1,9 @@
+package com.snacky.FoodOrderingApp_Back.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class CustomerProductController {
+}
