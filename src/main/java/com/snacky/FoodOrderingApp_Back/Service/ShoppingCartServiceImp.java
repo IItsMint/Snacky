a3e193fad8b8459faa90daa917129b5cbec5546e
kaan <kaanakman42@gmail.com>
2024-12-26 23:52:21 +0300

    add to shopping cart method implemented in service impl.

--- src/main/java/com/snacky/FoodOrderingApp_Back/Service/ShoppingCartServiceImp.java
+++ src/main/java/com/snacky/FoodOrderingApp_Back/Service/ShoppingCartServiceImp.java
@@ -1,16 +1,64 @@
 package com.snacky.FoodOrderingApp_Back.Service;
 
 import com.snacky.FoodOrderingApp_Back.Dto.AddToShoppingCartRequest;
+import com.snacky.FoodOrderingApp_Back.Model.Product.Product;
 import com.snacky.FoodOrderingApp_Back.Model.ShoppingCart.ShoppingCart;
+import com.snacky.FoodOrderingApp_Back.Model.ShoppingCart.ShoppingCartProduct;
+import com.snacky.FoodOrderingApp_Back.Model.User.User;
+import com.snacky.FoodOrderingApp_Back.Repository.ProductRepo;
+import com.snacky.FoodOrderingApp_Back.Repository.ShoppingCartProductRepo;
+import com.snacky.FoodOrderingApp_Back.Repository.ShoppingCartRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ShoppingCartServiceImp implements ShoppingCartService {
 
+    //we need to wire some instances,
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ShoppingCartRepo shoppingCartRepo;
+
+    @Autowired
+    private ShoppingCartProductRepo shoppingCartProductRepo;
+
+    @Autowired
+    private ProductService productService;
+
     //let's call the unimplemented methods.
     @Override
     public ShoppingCart addToShoppingCart(AddToShoppingCartRequest request, String jwt) throws Exception {
-        return null;
+
+        //first let's find the user.
+        User user = userService.findByJwtToken(jwt);
+
+        //now lets find the product.
+        Product product = productService.findProductById(request.getProductId());
+
+        //now we should find the shopping cart.
+        ShoppingCart shoppingCart = shoppingCartRepo.findByCustomerId(user.getId());
+
+        //now we need to create shopping cart product,
+        //first lets check if it is already exists in customer's shopping cart we update it.
+        for (ShoppingCartProduct shoppingCartProduct : shoppingCart.getItems()){
+
+            if (shoppingCartProduct.getProduct().getId() == product.getId()){
+                int newQuantity = shoppingCartProduct.getQuantity() + 1;
+                return updateShoppingCart(shoppingCartProduct.getId(), newQuantity);
+            }
+        }
+        //now if it is not exist we add it.
+        ShoppingCartProduct newShoppingCartProduct = new ShoppingCartProduct();
+        newShoppingCartProduct.setProduct(product);
+        newShoppingCartProduct.setCart(shoppingCart);
+        newShoppingCartProduct.setQuantity(request.getQuantity());
+        newShoppingCartProduct.setTotalPrice(request.getQuantity() * product.getPrice());
+
+        //now let's save it.
+        shoppingCartProductRepo.save(newShoppingCartProduct);
+        return shoppingCart;
     }
 
     @Override
