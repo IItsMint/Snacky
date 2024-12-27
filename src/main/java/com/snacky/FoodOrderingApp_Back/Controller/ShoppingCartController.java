912b30ba5c67c6dab759db3b80d54a4fd0ea9a5a
kaan <kaanakman42@gmail.com>
2024-12-28 00:36:28 +0300

    controller structured and method of adding to the shopping cart implemented.

---
+++ src/main/java/com/snacky/FoodOrderingApp_Back/Controller/ShoppingCartController.java
@@ -0,0 +1,27 @@
+package com.snacky.FoodOrderingApp_Back.Controller;
+
+import com.snacky.FoodOrderingApp_Back.Dto.AddToShoppingCartRequest;
+import com.snacky.FoodOrderingApp_Back.Model.ShoppingCart.ShoppingCartProduct;
+import com.snacky.FoodOrderingApp_Back.Service.ShoppingCartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class ShoppingCartController {
+
+    //let's first called to needed instances to wire them up.
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    @PutMapping("/shoppingCart/add")
+    public ResponseEntity<ShoppingCartProduct> addProductToShoppingCart(@RequestBody AddToShoppingCartRequest addToShoppingCartRequest,
+                                                                        @RequestHeader("Authorization") String jwt) throws Exception{
+
+        ShoppingCartProduct shoppingCartProduct = shoppingCartService.addToShoppingCart(addToShoppingCartRequest, jwt);
+
+        return new ResponseEntity<>(shoppingCartProduct, HttpStatus.OK);
+    }
+}
