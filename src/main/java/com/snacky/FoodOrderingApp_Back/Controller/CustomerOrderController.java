b110f790fcc5fa58a0ab070989210ea6a06c962a
kaan <kaanakman42@gmail.com>
2024-12-29 15:29:09 +0300

    controller for customers implemented.

--- src/main/java/com/snacky/FoodOrderingApp_Back/Controller/CustomerOrderController.java
+++ src/main/java/com/snacky/FoodOrderingApp_Back/Controller/CustomerOrderController.java
@@ -5,14 +5,17 @@ import com.snacky.FoodOrderingApp_Back.Model.Order.Order;
 import com.snacky.FoodOrderingApp_Back.Model.User.User;
 import com.snacky.FoodOrderingApp_Back.Service.OrderService;
 import com.snacky.FoodOrderingApp_Back.Service.UserService;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")//all the end points starting from api.
-public class OrderController {
+public class CustomerOrderController {
 
     //let's call instances that we are gonna use.
 
@@ -33,6 +36,18 @@ public class OrderController {
         Order order = orderService.createOrder(orderRequest, user);
 
         return new ResponseEntity<>(order, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/order/user")
+    public ResponseEntity<List<Order>> getAllUserOrder (@RequestHeader("Authorization") String jwt) throws Exception {
+
+        //let's find user first,
+        User user = userService.findByJwtToken(jwt);
+
+        //since one user will have more than one order, we use list.
+        List<Order> orders = orderService.getUsersOrder(user.getId());
+
+        return new ResponseEntity<>(orders, HttpStatus.OK);
     }
 
 }
